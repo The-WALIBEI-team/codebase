6a063cf067eabd73e01a1607b73924bd8a9331a2
System Administrator <root@MACs-MacBook-Air.local>
2021-01-15 15:09:29 +0530

    Bootstrap and Jquery integrated with the testing app, initial codes for the header and the footer

--- walibei-test/src/app/app.module.ts
+++ walibei-test/src/app/app.module.ts
@@ -3,10 +3,28 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { HomeComponent } from './home/home.component';
+import { ProductComponent } from './product/product.component';
+import { ServiceComponent } from './service/service.component';
+import { JobComponent } from './job/job.component';
+import { AccountComponent } from './account/account.component';
+import { LoginComponent } from './login/login.component';
+import { SignupComponent } from './signup/signup.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    HomeComponent,
+    ProductComponent,
+    ServiceComponent,
+    JobComponent,
+    AccountComponent,
+    LoginComponent,
+    SignupComponent
   ],
   imports: [
     BrowserModule,
