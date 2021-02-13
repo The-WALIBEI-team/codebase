563db8d306c1ad1d23a1a1e6c9453d4d4bcd1ea2
Samiratou Bonkoungou <bonkoungousamiratou75@gmail.com>
2021-02-13 13:42:59 +0530

    Header and footer

--- walibei/src/app/app-routing.module.ts
+++ walibei/src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HeaderComponent } from './header/header.component';
+import { AccueilComponent } from './accueil/accueil.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: "", pathMatch: "full",redirectTo: "home"},
+{path: "accueil", component: AccueilComponent },
+{path: "header", component: HeaderComponent},
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
